0957ce65073e5e31621d9d658b86b6b6e5baaf67
a.kato <a.kato.micron.inc@gmail.com>
2025-04-07 22:50:35 +0900

    UPDATE from home 2025/04/07

--- bin/my-lambda-s3-app.ts
+++ bin/my-lambda-s3-app.ts
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { MyLambdaS3AppStack } from '../lib/my-lambda-s3-app-stack';
+import { MyLambdaS3Stack } from '../lib/my-lambda-s3-app-stack';
 
 const app = new cdk.App();
-new MyLambdaS3AppStack(app, 'MyLambdaS3AppStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new MyLambdaS3Stack(app, 'MyLambdaS3Stack', {
+  /* 環境を明示的に指定することもできます
+  env: { 
+    account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: process.env.CDK_DEFAULT_REGION 
+  }
+  */
 });
--- lib/my-lambda-s3-app-stack.ts
+++ lib/my-lambda-s3-app-stack.ts
@@ -1,16 +1,68 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3'; // S3モジュールをインポート
+import * as iam from 'aws-cdk-lib/aws-iam'; // IAMモジュールをインポート
+import * as path from 'path';
 
-export class MyLambdaS3AppStack extends cdk.Stack {
+export class MyLambdaS3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3バケットの作成
+    const testBucket = new s3.Bucket(this, 'TestBucket', {
+      // バケット名を指定（オプション）。指定しない場合はCloudFormationによって一意の名前が生成されます
+      // bucketName: 'my-test-bucket-name', 
+      
+      // 様々なバケットプロパティを設定できます
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // スタック削除時にバケットも削除する
+      autoDeleteObjects: true, // バケット削除時に中のオブジェクトも自動削除
+      versioned: false, // バージョニングを無効化
+      publicReadAccess: false, // パブリックアクセスをブロック
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL, // すべてのパブリックアクセスをブロック
+      
+      // バケットライフサイクルルールの例（オプション）
+      lifecycleRules: [
+        {
+          id: 'DeleteAfter30Days',
+          expiration: cdk.Duration.days(30),
+          enabled: true,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyLambdaS3AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda関数の作成
+    const myFunction = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../src/lambda')),
+      environment: {
+        BUCKET_NAME: testBucket.bucketName, // 環境変数にバケット名を設定
+      },
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 256,
+    });
+
+    // Lambda関数にS3バケットへのアクセス権限を付与
+    testBucket.grantReadWrite(myFunction);
+
+    // または詳細な権限設定が必要な場合は以下のようにIAMポリシーを明示的に追加することもできます
+    /*
+    myFunction.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject', 's3:PutObject'],
+      resources: [testBucket.arnForObjects('*')],
+    }));
+    */
+
+    // スタックの出力値の設定
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: testBucket.bucketName,
+      description: '作成されたS3バケットの名前',
+    });
+
+    new cdk.CfnOutput(this, 'LambdaFunctionName', {
+      value: myFunction.functionName,
+      description: '作成されたLambda関数の名前',
+    });
   }
 }
